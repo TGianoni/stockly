afc5eb57fb44c21754641ee7e7479ae58073d9c1
TGianoni <gianoni2@hotmail.com>
2025-04-29 15:26:46 -0300

    feat: ensure sales and products page are scrollable

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -16,7 +16,7 @@ const ProductsPage = async () => {
   const products = await getProducts();
 
   return (
-    <div className="m-8 my-8 w-full space-y-8 rounded-lg bg-white p-8 px-8">
+    <div className="m-8 my-8 w-full space-y-8 overflow-auto rounded-lg bg-white p-8 px-8">
       <Header>
         <HeaderLeft>
           <HeaderSubtitle>Gestão de Produtos</HeaderSubtitle>
